9f703d24632607594d891f7ca25662ad6770005e
gemaadhan <gemaadhan@gmail.com>
2021-05-07 13:39:07 +0700

    #9 24:02

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -37,6 +37,22 @@ class Admin extends CI_Controller
         $this->load->view('admin/role', $data);
         $this->load->view('template/footer');
     }
+
+    public function roleAccess($role_id)
+    {
+        $data['title'] = 'Role';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['role'] = $this->db->get_where('user_role', ['id' => $role_id])->row_array();
+
+        $this->db->where('id !=', 1);
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar', $data);
+        $this->load->view('template/topbar', $data);
+        $this->load->view('admin/role-access', $data);
+        $this->load->view('template/footer');
+    }
 }
 
 /* End of file Admin.php */
