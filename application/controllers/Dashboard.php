523ceeee2243deab2922e6cb1ebb7872ba371ab1
gemaadhan <gemaadhan@gmail.com>
2021-07-02 06:10:52 +0700

    ingin rapi

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller
+{
+
+    public function index()
+    {
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('templates/topbar');
+        $this->load->view('dashboard');
+        $this->load->view('templates/footer');
+    }
+}
+
+/* End of file Dashboard.php */
